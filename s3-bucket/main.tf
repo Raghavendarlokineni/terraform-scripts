314ac92b4e1f3c07a832db35b2ca26fa91660d9f
Raghavendar Lokineni <raghavendralokineni@gmail.com>
2019-08-12 14:46:20 +0530

    adding file upload

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -10,7 +10,31 @@ resource "aws_s3_bucket" "testBucket" {
   versioning {
     enabled = true
   }
-  tags {
-    Name = "Testing Bucket"
+ 
+  lifecycle_rule {
+    enabled = true
+    transition {
+      days           = 30
+      storage_class = "STANDARD_IA"
+    }
+    transition {
+      days           = 60
+      storage_class  = "GLACIER"
+    }
+    expiration {
+      days = 90
+    }
+  } 
+ 
+  tags = {
+    Name = "Testing Bucket" 
   }
+}
+
+resource "aws_s3_bucket_object" "file_upload" {
+  bucket   = "tf-test-bucket-raghu"
+  key      = "autoupload"
+  source   = "FILE-PATH"
+  etag     = "${filemd5("FILE-PATH")}"
+
 }
